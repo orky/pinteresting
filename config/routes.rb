c529b067e698cc355dbfcaf880304036656fedb2
Sumedh Jigjinni <sjigjinni@gmail.com>
2014-05-12 14:08:02 -0700

    Added icons for buttons, changed root path, updated show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pages#home" #this is the root path, home
+  root "pins#index" #this is the root path, home
   get "about" => "pages#about" #creates about_path
 
   # The priority is based upon order of creation: first created -> highest priority.
